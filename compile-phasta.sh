2f504f1922b542b1bcaedb86c344e267c566896b
Matthew Mikhailov <mlmikhai@login01.expanse.sdsc.edu>
2022-08-22 19:29:55 -0700

    phasta script

---
+++ compile-phasta.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone https://github.com/PHASTA/phasta.git
+cd phasta
+module load cmake
+module load gcc/9.2.0
+module load openmpi/3.1.6
+if [ ! -d build ]; then
+	mkdir build
+fi
+cd build
+cmake ..
