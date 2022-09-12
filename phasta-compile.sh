8b1f64d520bb724c994c7813ab8cbeb0c6acd0ac
Rachel Handran <rachel.handran@gmail.com>
2022-09-11 20:30:22 -0700

    fixing phasta v1

--- phasta-compile.sh
+++ phasta-compile.sh
@@ -2,8 +2,9 @@
 git clone https://github.com/PHASTA/phasta.git
 cd phasta
 module purge
-module load cmake
+module load cpu/0.15.4
 module load gcc/9.2.0
+module load cmake/3.18.2
 module load openmpi/3.1.6
 if [ ! -d build ]; then
 	mkdir build
