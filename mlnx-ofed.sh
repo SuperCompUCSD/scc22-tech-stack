9c30db3e541978681341f551c1643c67843a435f
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-10-28 05:41:08 +0000

    remove sudoing in mlnx script

--- mlnx-ofed.sh
+++ mlnx-ofed.sh
@@ -3,4 +3,4 @@
 wget https://www.mellanox.com/downloads/ofed/MLNX_OFED-5.7-1.0.2.0/MLNX_OFED_SRC-debian-5.7-1.0.2.0.tgz
 tar -xvzf MLNX_OFED_SRC-debian-5.7-1.0.2.0.tgz
 cd MLNX_OFED_SRC-5.7-1.0.2.0/
-sudo ./install.pl
+./install.pl
