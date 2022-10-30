393d96a596ab61660f2b9f0c24c00db1e71ce4dc
Michael Granado <magranado@ucsd.edu>
2022-10-30 06:32:09 +0000

    Build Phasta w/ or w/o PETSC

--- phasta-scripts/compile-phasta.sh
+++ phasta-scripts/compile-phasta.sh
@@ -1,15 +1,37 @@
-#!/bin/bash
-echo "Checking system for gcc, openmpi, cmake, Paraview and Gmsh..."
-which gcc
-which mpirun
-which cmake
-which paraview
-which gmsh
+#!/bin/bash -e
+echo "Checking system for Gmsh..."
+GMSH=$(which gmsh)
 
-echo "Running script to build Phasta..."
-. ./build-phasta.sh
+if [ $GMSH != "/usr/bin/gmsh" ]
+then
+	echo "Installing GMSH"
+	cd
+	wget https://gmsh.info/bin/Linux/gmsh-4.10.5-Linux64.tgz
+	tar -xvf gmsh-4.10.5-Linux64.tgz
+	sudo mv gmsh-4.10.5-Linux64/bin/* /usr/bin/
+	sudo mv gmsh-4.10.5-Linux64/share/doc/gmsh /usr/share/doc/
+	sudo mv gmsh-4.10.5-Linux64/share/man/man1/gmsh.1 /usr/share/man/man1/
+	rm -r gmsh-4.10.5-Linux64
+	rm gmsh-4.10.5-Linux64.tgz
+	echo "GMSH Installed"
+else
+	echo "GMSH Already Installed"
+fi
+
+cd ~/scc22-scripts/phasta-scripts
+
+echo "Press 1 to Build Phasta with PETSC and 0 to Build Phasta without PETSC"
+read PETSC_YES
+
+if [ ${PETSC_YES} == "1" ]
+then
+	echo "Starting Build for Phasta with PETSC..."
+ 	./phasta-petsc.sh
+else
+	echo "Starting Build for Phasta without PETSC..."
+        ./phasta-nopetsc.sh
+fi
+cd ~/scc22-scripts/phasta-scripts
 echo "Running script to build Chef..."
-. ./chef-install.sh
-echo "Running script to build PETSC..."
-. ./petsc-phasta.sh
-echo "Done compiling. Use the run.sh script to run PHASTA"
+# ./chef-install.sh
+#echo "Done compiling. Use the run.sh script to run PHASTA"
---
+++ phasta-scripts/phasta-nopetsc.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+echo "Installing Phasta without PETSC..."
+cd
+if [ -d "$HOME/phasta" ];
+then
+        sudo rm -r phasta
+fi
+git clone git@github.com:PHASTA/phasta.git
+cd phasta
+mkdir build
+cd build
+MPIEXEC=(which mpiexec)
+cmake \
+-DCMAKE_C_COMPILER=gcc \
+-DCMAKE_CXX_COMPILER=g++ \
+-DCMAKE_Fortran_COMPILER=gfortran \
+-DCMAKE_Fortran_FLAGS=-fallow-argument-mismatch \
+-DCMAKE_BUILD_TYPE=Debug \
+-DPHASTA_INCOMPRESSIBLE=OFF \
+-DPHASTA_COMPRESSIBLE=ON \
+-DPHASTA_USE_SVLS=OFF \
+-DPHASTA_USE_PETSC=OFF \
+-DMPIEXEC_EXECUTABLE="$MPIEXEC" \
+..
+echo "Building Phasta executable..."
+make -j phastaC.exe
+
+cd ~/phasta
+echo "Getting phastaChef tests..."
+wget https://fluid.colorado.edu/~kjansen/PHASTA/phastaChefTests.tar.gz
+tar -xzf phastaChefTests.tar.gz
