0e3e64e678bf0655bc70cb1c58aaf3cbb96aa62a
Yuchen Jing <y5jing@ucsd.edu>
2022-10-31 06:06:13 +0000

    Add: better add-users.sh and scripts to add all users

--- add-user.sh
+++ add-user.sh
@@ -1,7 +1,13 @@
 #!/bin/sh -e
-if [ "$#" -ne 3 ]; then
-	echo "Usage: $0 <username> <SSH key> <sudo | normal>"
+USAGE="Usage: $0 <username> <SSH key> <sudo | normal> [<uid> <gid>]"
+if [ "$#" -ne 3 ] && [ "$#" -ne 5 ]; then
+	echo "$USAGE"
 	exit 1
+fi
+if [ "$#" -eq 5 ]; then
+	set_ids=true
+	uid="$4"
+	gid="$5"
 fi
 user="$1"
 key="$2"
@@ -9,19 +15,24 @@ case "$3" in
 	"sudo") super=true;;
 	"normal") super=false;;
 	*)
-		echo "Usage: $0 <username> <SSH key> <sudo | normal>"
+		echo "$USAGE"
 		exit 1
 		;;
 esac
 
-sudo useradd -ms /bin/bash "$user"
+if [ "$set_ids" = true ]; then
+	sudo useradd -ms /bin/bash "$user" -u "$uid"
+	sudo groupmod -g "$gid" "$user"
+else
+	sudo useradd -ms /bin/bash "$user"
+fi
 sudo passwd -de "$user"
 if [ "$super" = true ]; then
 	sudo usermod -aG ssh-users,sudo "$user"
 else
 	sudo usermod -aG ssh-users "$user"
 fi
-sudo mkdir /home/"$user"/.ssh
+sudo mkdir -p /home/"$user"/.ssh
 echo "$key" | sudo tee /home/"$user"/.ssh/authorized_keys > /dev/null
 sudo chmod 600 /home/"$user"/.ssh/authorized_keys
 sudo chmod 700 /home/"$user"/.ssh
