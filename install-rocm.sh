6dee0d762028de6ec3c64b2025eba8cc1fd96fff
Khai Vu <khaitvu@gmail.com>
2022-11-01 15:44:14 -0700

    removed redundancy

--- install-rocm.sh
+++ install-rocm.sh
@@ -1,8 +1,15 @@
-#!/bin/bash -e
-
-sudo apt-get update
-sudo apt-get install gnupg2
-for ID in $(getent passwd {1000..2000} | awk -F ':' '{ print $1 }') ; do sudo usermod -aG render "$ID"; sudo usermod -aG video "$ID"; done
-wget https://repo.radeon.com/amdgpu-install/5.3/ubuntu/focal/amdgpu-install_5.3.50300-1_all.deb
-apt-get install ./amdgpu-install_5.3.50300-1_all.deb
-
+sudo apt update -y && \
+	echo -ne 'adding rocm_gpg' && \
+	wget -q -O - https://repo.radeon.com/rocm/rocm.gpg.key | sudo apt-key add - && \
+	echo -ne 'adding adding amdgpu to apt' && \
+	echo 'deb [arch=amd64] https://repo.radeon.com/amdgpu/latest/ubuntu focal main' | sudo tee /etc/apt/sources.list.d/amdgpu.list && \
+	echo 'deb [arch=amd64] https://repo.radeon.com/rocm/apt/debian/ focal main' | sudo tee /etc/apt/sources.list.d/rocm.list && \
+	echo -e 'Package: *\nPin: release o=repo.radeon.com\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/rocm-pin-600 && \
+	echo -ne 'updating' && \
+	sudo apt-get update -y && \
+	echo -ne 'installing rocm and drivers' && \
+	sudo apt install -y rocm-hip-sdk rocm-opencl-sdk amdgpu-dkms && \
+	echo -ne 'creating rocm_smi' && \
+	sudo ln -s /opt/rocm*/libexec/rocm_smi/rocm_smi.py /usr/bin/ && \
+	sudo mv /usr/bin/rocm_smi.py /usr/bin/rocm_smi && \
+	echo $(tput bold; tput setaf 196)  YOUR\'RE GOING TO NEED TO REBOOT $(tput sgr0)
