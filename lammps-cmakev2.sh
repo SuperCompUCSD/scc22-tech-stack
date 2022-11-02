016810939dd30a24fa3212df694029d913e4cc74
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-02 20:35:19 +0000

    add broken lammps cmake scripts

--- lammps-cmakev2.sh
+++ lammps-cmakev2.sh
@@ -4,21 +4,21 @@ export hipcc=$(which hipcc)
 export install_prefix=.
 export mpi_cxx=$(which mpicxx)
 cmake \
-  -DPKG_KOKKOS=on \
-  -DPKG_REAXFF=on \
-  -DPKG_MANYBODY=on \
-  -DPKG_ML-SNAP=on \
-  -DBUILD_MPI=on \
+  -DPKG_KOKKOS=yes \
+  -DPKG_REAXFF=yes \
+  -DPKG_MANYBODY=yes \
+  -DPKG_ML-SNAP=yes \
+  -DBUILD_MPI=yes \
   -DCMAKE_INSTALL_PREFIX=${install_prefix} \
   -DMPI_CXX_COMPILER=${mpi_cxx} \
-  -DKokkos_ENABLE_HIP=on \
-  -DKokkos_ENABLE_SERIAL=on \
-  -DBUILD_OMP=off \
+  -DKokkos_ARCH_ZEN3=yes \
+  -DKokkos_ARCH_VEGA90A=yes \
+  -DKokkos_ENABLE_HIP=yes \
   -DCMAKE_CXX_STANDARD=14 -DKokkos_CXX_STANDARD=14 \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
   -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
   -DCMAKE_CXX_COMPILER=${HIP_PATH}/bin/hipcc \
-  -DKokkos_ARCH_ZEN3=ON \
-  -DKokkos_ARCH_VEGA90A=ON \
   ../cmake
 
+#  -DBUILD_OMP=yes \
+#  -DKokkos_ENABLE_OPENMP=yes \
