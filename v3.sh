efe555292716b9748b83e6f3043d2d90c3b05bac
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-05 02:49:53 +0000

    add openmp script

--- v3.sh
+++ v3.sh
@@ -1,8 +1,8 @@
 cmake \
   	-DPKG_KOKKOS=yes \
-	-DCMAKE_CXX_COMPILER=hipcc \
+	-DCMAKE_CXX_COMPILER=g++ \
 	-DCMAKE_INSTALL_PREFIX=/home/mlmikhai \
-	-DKokkos_ENABLE_HIP=on \
+	-DKokkos_ENABLE_OPENMP=yes \
+	-DBUILD_OMP=yes \
 	-DKokkos_ARCH_VEGA90A=on \
-	-DKokkos_ARCH_ZEN3=on \
 	../cmake
