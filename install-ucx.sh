32ee976e22fb328387c5cdd719ca63053e260c9d
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-06 06:07:47 +0000

    ucx install script

--- install-ucx.sh
+++ install-ucx.sh
@@ -2,12 +2,12 @@
 
 cd ~
 MYHOME=$(pwd)
-mkdir -p matt/local
+mkdir -p matt/local/ucx
 cd matt
 git clone -b v1.14.x https://github.com/openucx/ucx.git
 cd ucx
 ./autogen.sh
-./contrib/configure-release --prefix=${MYHOME}/matt/local
+./contrib/configure-release --prefix=${MYHOME}/matt/local/ucx
 make -j8
 make install
 
