2f54d834f531208749844bf7413e4f952e000538
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-06 05:40:48 +0000

    fix ompi install with roce

--- install-roce-mpi.sh
+++ install-roce-mpi.sh
@@ -2,11 +2,11 @@
 
 cd ~
 MYHOME=$(pwd)
-mkdir -p matt/local
+mkdir -p matt/local/ompi
 cd matt
 wget https://download.open-mpi.org/release/open-mpi/v4.1/openmpi-4.1.4.tar.gz
 tar -xvzf openmpi-4.1.4.tar.gz
 cd openmpi-4.1.4
-./configure --prefix=${MYHOME}/matt/local --with-ucx=/usr/ --with-verbs=/usr/ --with-verbs-libdir=/usr/lib/x86_64-linux-gnu/
+./configure --prefix=${MYHOME}/matt/local/ompi --with-ucx=${MYHOME}/matt/local/ucx
 make all install -j 128
 
