f4585a70aa4a40a09e667d1e089ccba78a5f0f33
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-15 13:50:52 +0000

    clean up lammps branch in prep for merge

--- lammps-cmake.sh
+++ lammps-cmake.sh
@@ -1,24 +1,10 @@
-#!/bin/bash
-source /etc/profile
-export hipcc=$(which hipcc)
-export install_prefix=.
-export mpi_cxx=$(which mpicxx)
 cmake \
-  -DPKG_KOKKOS=on \
-  -DPKG_REAXFF=on \
-  -DPKG_MANYBODY=on \
-  -DPKG_ML-SNAP=on \
-  -DBUILD_MPI=on \
-  -DCMAKE_INSTALL_PREFIX=${install_prefix} \
-  -DMPI_CXX_COMPILER=${mpi_cxx} \
-  -DKokkos_ENABLE_HIP=on \
-  -DKokkos_ENABLE_SERIAL=on \
-  -DBUILD_OMP=off \
-  -DCMAKE_CXX_STANDARD=14 -DKokkos_CXX_STANDARD=14 \
-  -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-  -DCMAKE_EXE_LINKER_FLAGS="${LDFLAGS}" \
-  -DCMAKE_CXX_COMPILER=${HIP_PATH}/bin/hipcc \
-  -DKokkos_ARCH_ZEN3=ON \
-  -DKokkos_ARCH_VEGA90A=ON \
-  ../cmake
-
+  	-DPKG_KOKKOS=yes \
+	-DCMAKE_CXX_COMPILER=hipcc \
+	-DCMAKE_INSTALL_PREFIX=/home/mlmikhai \
+	-DKokkos_ENABLE_HIP=on \
+	-DKokkos_ENABLE_OPENMP=yes \
+	-DBUILD_OMP=yes \
+	-DKokkos_ARCH_VEGA90A=on \
+	-DKokkos_ARCH_ZEN3=on \
+	../cmake
--- v3.sh
+++
@@ -1,8 +0,0 @@
-cmake \
-  	-DPKG_KOKKOS=yes \
-	-DCMAKE_CXX_COMPILER=g++ \
-	-DCMAKE_INSTALL_PREFIX=/home/mlmikhai \
-	-DKokkos_ENABLE_OPENMP=yes \
-	-DBUILD_OMP=yes \
-	-DKokkos_ARCH_VEGA90A=on \
-	../cmake
