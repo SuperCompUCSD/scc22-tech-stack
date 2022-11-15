ea6b13ae7bb907befc519e6e810af39a1e793034
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-15 17:19:14 +0000

    add lammps scripts

---
+++ lammps-cmake-index.sh
@@ -0,0 +1,1 @@
+cmake -C ~/scc22-scripts/2mclammps.cmake -DLAMMPS_SIZES=smallsmall ../cmake
---
+++ lammps-compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if ! [[ -e ./lammps-23Jun2022 ]]; then
+  tar -xvzf lammps-stable.tar.gz
+  cd lammps-23Jun2022
+  mkdir build
+  cd build
+  source ${1}
+  cmake --build . -j 128
+else
+  printf "\nlammps-23Jun2022 directory already exists, please move to prevent overwriting\n"
+fi
+
