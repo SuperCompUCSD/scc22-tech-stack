9b12d17d75f93588af21e24ded7fab7292a2fd40
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-16 01:21:25 +0000

    no fast math

--- lammps-cmake-fmath.sh
+++ lammps-cmake-fmath.sh
@@ -1,3 +1,3 @@
 
-cmake -C ~/scc22-scripts/optlammps.cmake -DCMAKE_CXX_FLAGS=" -g -O3 -ffast-math" -DLAMMPS_SIZES=bigbig ../cmake
+cmake -C ~/scc22-scripts/optlammps.cmake -DCMAKE_CXX_FLAGS=" -g -O3" -DLAMMPS_SIZES=bigbig ../cmake
 # -DCMAKE_CXX_FLAGS="-g -O3 -sdtd=c++11" 
