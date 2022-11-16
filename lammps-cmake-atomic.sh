ed1124873db647fd7507cd29d5aefed9e401870e
Matthew Mikhailov <mlmikhai@ucsd.edu>
2022-11-16 04:04:38 +0000

    fix atomics error

--- lammps-cmake-atomic.sh
+++ lammps-cmake-atomic.sh
@@ -2,4 +2,4 @@
 #You should bind your MPI tasks by passing "--bind-to <some binding>" to the mpiexec command
 # You should bind OpenMP threads by setting environment variables OMP_PROC_BIND=spread and OMP_PLACES=threads in your runscript. These are environment variables, not cmake variables, and you should export them.
 # To take advantage of LMP_KOKKOS_USE_ATOMICS, you have to use half neighbor lists
-cmake -C ~/scc22-scripts/optlammps.cmake -DKokkos_ENABLE_HWLOC=on -DLAMMPS_SIZES=bigbig -DLMP_KOKKOS_USE_ATOMICS ../cmake
+cmake -C ~/scc22-scripts/optlammps.cmake -DKokkos_ENABLE_HWLOC=on -DLAMMPS_SIZES=bigbig -DLMP_KOKKOS_USE_ATOMICS=on ../cmake
